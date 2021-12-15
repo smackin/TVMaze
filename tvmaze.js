3fe7fdb5d9916442f99b7d9f8adc1f035394ebcd
Stephanie <smmackin12@gmail.com>
2021-12-15 10:07:00 -0500

    second push

--- tvmaze.js
+++ tvmaze.js
@@ -17,27 +17,28 @@
       }
  */
 
-     const searchAPI =  "http://api.tvmaze.com/search/shows?q=`${searchInput}`"
+    
     
      async function searchShows(query) {
   // TODO: Make an ajax request to the searchShows api.  Remove
   // hard coded data.
-      const response = await axios.get("http://api.tvmaze.com/search/shows?q=girls"); 
-      let data = response.data //object
-      let dataArray =  (Object.entries(data))
-      for (let shows of dataArray){
-        console.log(shows)
-      }
-
-      
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+      const response = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`); 
+      let data = response.data.map(result => {
+        let show = result.show; 
+        return {
+          id : show.id, 
+          name: show.name, 
+          summary: show.summary, 
+          image: show.image 
+        };
+      }); 
+ 
+    // let dataArray =  (Object.entries(data))
+    //   for (let shows of dataArray){
+    //     console.log(shows)
+    //   }
+  
+  return shows; 
 }
 
 /** Populate shows list:
@@ -94,4 +95,7 @@ async function getEpisodes(id) {
   //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
 
   // TODO: return array-of-episode-info, as described in docstring above
+
+  
+
 }
--- tvmaze2.js
+++ tvmaze2.js
@@ -2,7 +2,8 @@ const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async function(e){
     e.preventDefault(); 
     const searchTerm = form.elements.query.value; 
-    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`); 
+    const params = {params: {q:searchTerm, }} // each key-value pair will be added to the query string.   
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=`, params); 
    // console.log(res.data) // res.data returns an array of objects from API.  
     //console.log(res.data[0].show.image.medium); // to return the image of index 0 - use the show.image.medium element
     //console.log(res.data[2].show.status);
@@ -10,19 +11,21 @@ form.addEventListener('submit', async function(e){
     // const img = document.createElement('IMG'); 
     // img.src = (res.data[0].show.image.medium)
     // document.body.append(img);
-    makeImages(res.data);
-    form.elements.query.value=''; 
+    makeImages(res.data);// function created below to make image - the images are being made from the res.data element that was returned by the API.   
+    form.elements.query.value = ''; // emptying the input after the submit request is run. 
 });
 // to display all images for search results loop over res.data, which is an array,  and then for each result - create an image and append it. make a new function for this.  You would take the make image portion of the function and make a new function to call insude of form submission.   
 
 function makeImages(shows){ 
 for (let result of shows){
-    if(result.show.image.medium){
+    if(result.show.image){
     const img = document.createElement('IMG'); 
-    img.src = (result.show.image.medium)  
+    img.src = result.show.image.medium;  
     document.body.append(img);
     }
   }
 }
+
+
 
 
